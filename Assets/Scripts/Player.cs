7ebe7145ec828945c81fcbd38dbd18b8dcbc8b55
Michael Hatfield <michaellandonhatfield@gmail.com>
2022-04-12 14:18:34 -0400

    PlayerMovement Completed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,30 +15,36 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	    float horizontalInput = Input.GetAxis("Horizontal");
-	    float verticalInput = Input.GetAxis("Vertical");
-	    
-	    Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-	    transform.Translate(direction * _speed * Time.deltaTime);
+		CalculateMovement();
+    }
+
+	void CalculateMovement()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+
+		Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+		transform.Translate(direction * _speed * Time.deltaTime);
 
 		//check the vertical position and adjust as necessary to keep it between 0 and -3.8
-	    if (transform.position.y >= 0)
-	    {
-		    transform.position = new Vector3(transform.position.x, 0, transform.position.z);
-	    }
-		else if(transform.position.y < -4)
-        {
+		if (transform.position.y >= 0)
+		{
+			transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+		}
+		else if (transform.position.y < -4)
+		{
 			transform.position = new Vector3(transform.position.x, -4, transform.position.z);
-        }
+		}
 
 		//check the horizontal position - if the player goes off screen then wrap to other side
-		if(transform.position.x > 11)
-        {
-			transform.position = new Vector3(-11, transform.position.y, transform.position.z);
-        }
-		else if(transform.position.x < -11)
-        {
-			transform.position = new Vector3(11, transform.position.y, transform.position.z);
-        }
-    }
+		if (transform.position.x > 11.3f)
+		{
+			transform.position = new Vector3(-11.3f, transform.position.y, transform.position.z);
+		}
+		else if (transform.position.x < -11.3f)
+		{
+			transform.position = new Vector3(11.3f, transform.position.y, transform.position.z);
+		}
+	}
+
 }
