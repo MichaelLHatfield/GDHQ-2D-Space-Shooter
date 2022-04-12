5a15b05e6178a80f0d914b14289e4242d2998cbc
Michael Hatfield <michaellandonhatfield@gmail.com>
2022-04-12 11:50:34 -0400

    PlayerMovement Complete

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	[SerializeField]
-	private float _speed = 3.5f;
+	[SerializeField] private float _speed = 3.5f;
 	
     // Start is called before the first frame update
     void Start()
     {
-        //take current position and assign it the start position (0,0,0)
         transform.position = new Vector3(0, 0, 0);
-
     }
 
     // Update is called once per frame
@@ -21,8 +18,27 @@ public class Player : MonoBehaviour
 	    float horizontalInput = Input.GetAxis("Horizontal");
 	    float verticalInput = Input.GetAxis("Vertical");
 	    
-	    transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-	    transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
-	    
+	    Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+	    transform.Translate(direction * _speed * Time.deltaTime);
+
+		//check the vertical position and adjust as necessary to keep it between 0 and -3.8
+	    if (transform.position.y >= 0)
+	    {
+		    transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+	    }
+		else if(transform.position.y < -4)
+        {
+			transform.position = new Vector3(transform.position.x, -4, transform.position.z);
+        }
+
+		//check the horizontal position - if the player goes off screen then wrap to other side
+		if(transform.position.x > 11)
+        {
+			transform.position = new Vector3(-11, transform.position.y, transform.position.z);
+        }
+		else if(transform.position.x < -11)
+        {
+			transform.position = new Vector3(11, transform.position.y, transform.position.z);
+        }
     }
 }
